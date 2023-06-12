2fb3382c02c05f1b73a7242b88c879b4e5eff47f
matteopasqualetti <matteo.pasqualetti@engicam.com>
2023-06-12 11:11:05 +0200

    i.Core MX6 Solo + Solo eMMC: added support

--- arch/arm/mach-imx/mach-imx6q.c
+++ arch/arm/mach-imx/mach-imx6q.c
@@ -13,6 +13,8 @@
 #include <linux/micrel_phy.h>
 #include <linux/mfd/syscon.h>
 #include <linux/mfd/syscon/imx6q-iomuxc-gpr.h>
+#include <linux/of_net.h>
+#include <linux/of_gpio.h>
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
 
@@ -20,6 +22,7 @@
 #include "cpuidle.h"
 #include "hardware.h"
 
+static int edimm_ver = 10;
 /* For imx6q sabrelite board: set KSZ9021RN RGMII pad skew */
 static int ksz9021rn_phy_fixup(struct phy_device *phydev)
 {
@@ -76,6 +79,37 @@ static void __init imx6q_enet_phy_init(void)
 	}
 }
 
+#define ICORE_GPIO_EDIMM_VER 191 /* GPIO6_31 */
+static void __init icore_set_enet_clock(struct regmap *gpr)
+{
+	int icore_ver_gpio;
+	icore_ver_gpio = ICORE_GPIO_EDIMM_VER;
+
+	if (gpio_is_valid(icore_ver_gpio) &&
+		gpio_request_one(icore_ver_gpio, GPIOF_DIR_IN, "icore_ver_gpio")) {
+
+
+		gpio_direction_input(icore_ver_gpio);
+		if(!gpio_get_value(icore_ver_gpio))
+		{
+			edimm_ver = 15;
+				printk("i.Core M6 1.5 version\n");
+				regmap_update_bits(gpr, IOMUXC_GPR1,
+						IMX6Q_GPR1_ENET_CLK_SEL_MASK,
+						IMX6Q_GPR1_ENET_CLK_SEL_ANATOP);
+		}
+		else
+		{
+			edimm_ver = 10;
+			printk("i.Core M6 standard 1.0 version\n");
+			regmap_update_bits(gpr, IOMUXC_GPR1,
+					IMX6Q_GPR1_ENET_CLK_SEL_MASK,
+					IMX6Q_GPR1_ENET_CLK_SEL_PAD);
+		}
+		gpio_free(icore_ver_gpio);
+	}
+}
+
 static void __init imx6q_1588_init(void)
 {
 	struct device_node *np;
@@ -112,9 +146,15 @@ static void __init imx6q_1588_init(void)
 				IMX6Q_GPR1_ENET_CLK_SEL_PAD;
 	gpr = syscon_regmap_lookup_by_compatible("fsl,imx6q-iomuxc-gpr");
 	if (!IS_ERR(gpr))
-		regmap_update_bits(gpr, IOMUXC_GPR1,
-				IMX6Q_GPR1_ENET_CLK_SEL_MASK,
-				clksel);
+	{
+		if (of_machine_is_compatible("fsl,imx6q") ||
+		of_machine_is_compatible("fsl,imx6dl") )
+			icore_set_enet_clock(gpr);
+		else
+			regmap_update_bits(gpr, IOMUXC_GPR1,
+					IMX6Q_GPR1_ENET_CLK_SEL_MASK,
+					clksel);
+	}
 	else
 		pr_err("failed to find fsl,imx6q-iomuxc-gpr regmap\n");
 
